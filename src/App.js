0cd73730d543a8e33c111fc3018cc0877a2d0b2e
Marcos <marcosarantes.contato@hotmail.com>
2024-06-02 23:37:40 -0300

    cloud front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Desafio SpotSat - Aplicação React usando CloudFront</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
